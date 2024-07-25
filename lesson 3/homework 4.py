12a20b13bcb679a71695ae439941eb482af762c5
ruslan <ruslanpostolskii@i.ua>
2024-07-25 23:30:09 +0300

    home-work math 4 list-2.py

--- lesson 3/homework 4.py
+++ lesson 3/homework 4.py
@@ -1,15 +1,11 @@
-lst1 = [4, 5, 6, 7, 8, 9] #було [4, 5, 6, 7, 8, 9] стало [[4, 5, 6], [7, 8, 9]]
-lst2 = ([lst1[0:3], lst1[3:]])
-print(lst2)
-lst1 = [4, 5, 6] #було [4, 5, 6] стало [[4, 5], [6]]
-lst3 = ([lst1[0:2], lst1[2:3]])
-print(lst3)
-lst1 = [4, 5, 6, 7, 8] #було [4, 5, 6, 7, 8] стало [[4, 5, 6], [7, 8]]
-lst4 = ([lst1[0:3], lst1[3:5]])
-print (lst4)
-lst1 = [4] #було [4] стало [4], []]
-lst5 = ([lst1[0:], lst1[:0]])
-print(lst5)
-lst1 = [] #було [] стало[[], []]
-lst6 = ([lst1[:0], lst1[:0]])
-print(lst6)
+lst1 = [4, 5, 6, 7, 8, 9, 8, 7]
+lst2 = len(lst1) //2
+print([lst1[:lst2], lst1[lst2:]])
+lst3 = [4, 5, 6]
+lst4 = len(lst3) //2
+print([lst3[:lst4], lst3[lst4:]])
+lst5 = []
+lst6 = len(lst5) //2
+print([lst5[:lst6], lst5[lst6:]])
+
+
