6048d51b20acf372c5ee07629d82d10cfd52c32d
ruslan <ruslanpostolskii@i.ua>
2024-08-06 11:07:53 +0300

    homework 7 calculator #

--- lesson 2/calcelator.py
+++ lesson 2/calcelator.py
@@ -1,15 +1,27 @@
-a = int(input('напишіть число'))
-operation = input('введіть операцію: +, -, *, /')
-b = int(input('напишіть друге число'))
-if operation == '+':
-    result = (a + b)
-    print(result)
-elif operation == '-':
-    result = (a - b)
-    print(result)
-elif operation == '*':
-    result = (a * b)
-    print(result)
-elif operation == '/':
-    result = (a / b)
-    print(result)
+while True:
+    a = int(input('напишіть число'))
+    operation = input('введіть операцію: +, -, *, /')
+    b = int(input('напишіть друге число'))
+    if operation == '+':
+        result = (a + b)
+        print(result)
+        if operation == '-':
+            result = (a - b)
+            print(result)
+            if operation == '*':
+                result = (a * b)
+                print(result)
+            elif operation == '/':
+                if b != 0:
+                    print(a / b)
+    else:
+        print('Ділення на нуль')
+        break
+    if input("Для продовження операцій натисніть'y'?") != "y":
+        break
+
+
+
+
+
+
