c01bd41309fcc91462138d3a60332d0c2983f208
ruslan <ruslanpostolskii@i.ua>
2024-08-06 18:45:39 +0300

    homework 7 calculator #

--- homework 6.#.py
+++ homework 6.#.py
@@ -1,6 +1,5 @@
 a = (input("")[:140])
 a = a.title()
-while " " in a:
-    a = a.replace(' ', '').replace('%', '').replace('$', '').replace('@', '').replace('*', '').replace('.', '').replace('?', '').replace('&', '').replace('!', '').replace(',', '')
+a = a.replace(' ', '').replace('', '').replace('&', '').replace('#', '').replace('"', '').replace('~', '').replace('%', '').replace('$', '').replace('@', '').replace('*', '').replace('.', '').replace('?', '').replace('&', '').replace('!', '').replace(',', '')
 
 print('#' + a)
